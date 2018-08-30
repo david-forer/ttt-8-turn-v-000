34e818080b96f9bde95dbe02fd2fed2940d25979
David Forer <david.j.forer@gmail.com>
2018-08-30 13:35:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,6 +27,7 @@ end
 def position_taken? (board, index)
   board[index] != " " && board[index] != "" && board[index] != nil
 end
+
 def valid_move?(board, index)
    if index.between?(0,8) && !position_taken?(board,index)
      true
