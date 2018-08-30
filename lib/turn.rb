fc27ea7a522dd4b3bb2549cfecb374570dad1001
David Forer <david.j.forer@gmail.com>
2018-08-30 13:28:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,5 +16,12 @@ def turn(board)
   puts "Please enter 1-9: "
 end
 
-
+def position_taken? (board, index)
+  board[index] != " " && board[index] != "" && board[index] != nil
+end
+def valid_move?(board, index)
+   if index.between?(0,8) && !position_taken?(board,index)
+     true
+   end
+end
 
